525cff07492059a918d0d8c66ce39a55d21949b0
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-06-27 20:15:52 -0400

    Create cloud.js

---
+++ cloud.js
@@ -0,0 +1,44 @@
+const { Client, Collection, RichEmbed } = require("discord.js");
+const fs = require("fs");
+const client = new Client({
+  disableEveryone: true
+});
+client.login(process.env.TOKEN);
+client.commands = new Collection();
+client.aliases = new Collection();
+client.categories = fs.readdirSync("./commands/");
+
+["command"].forEach(handler => {
+  require(`./handlers/${handler}`)(client);
+});
+client.on("ready", async function() {
+  var list = [
+    `Use my Prefix ${process.env.PREFIX}`,
+    `On ${client.guilds.size} servers `,
+`A Cloud Bot`
+  ];
+  setInterval(function() {
+    const Exec = Math.floor(Math.random() * list.length);
+    client.user.setActivity(list[Exec], { type: "STREAMING" });
+    console.log(Exec);
+  }, 10000);
+
+  console.log("online  "+client.user.tag);
+});
+client.on("message", async message => {
+  const prefix = process.env.PREFIX;
+  if (message.author.bot) return;
+  if (!message.guild) return;
+  if (!message.content.startsWith(prefix)) return;
+  if (!message.member)
+    message.member = await message.guild.fetchMember(message);
+  const args = message.content
+    .slice(prefix.length)
+    .trim()
+    .split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+  if (cmd.length === 0) return;
+  let command = client.commands.get(cmd);
+  if (!command) command = client.commands.get(client.aliases.get(cmd));
+  if (command) command.run(client, message, args);
+});
