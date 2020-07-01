57678d72d53d67b47d3a15846f7d12d7a064cc7d
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-07-01 19:13:31 -0400

    Update RainbowRole.js

--- commands/RoleManagement/RainbowRole.js
+++ commands/RoleManagement/RainbowRole.js
@@ -9,7 +9,7 @@ module.exports = {
       return message.reply("You don't have the required permissions to use this command.").then(m => m.delete(5000));
       let text = args[0]
       if(!text.startsWith("#")) return message.reply("Hex colors starts with #")
-    if(text.length > "6") return message.reply("Must be 6 values in length")
+    if(text.length > "7") return message.reply("Must be 6 values in length")
       let ge = args[1]
       if(!text)return message.reply("please specify hex color")
     let rRole= message.guild.roles.find("name", ge)
