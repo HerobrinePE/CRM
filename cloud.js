9e44b1d842f26b752a93667602ae68ad3484492d
HerobrinePE <42194169+HerobrinePE@users.noreply.github.com>
2020-07-07 17:31:05 -0400

    Update cloud.js

--- cloud.js
+++ cloud.js
@@ -23,7 +23,7 @@ client.on("ready", async function() {
     console.log(Exec);
   }, 10000);
  
-  console.log("online  "+client.user.tag+client.guild.size);
+  console.log("online  "+client.user.tag+client.guilds.size);
 });
 client.on("message", async message => {
   const prefix = process.env.PREFIX;
